c55d0d630a7797a4c5d7d384901ff31801bd61dd
Juanlu Belda <juanluis.belda@gmail.com>
2019-10-11 18:57:43 +0200

    develop branch

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:name', function(req, res, next) {
+  res.send(`Hola ${req.params.name}`);
+});
+
 module.exports = router;
